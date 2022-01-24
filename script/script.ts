fab9e8b1e4b6be4ac3f33320bfda465e38935dce
PenelopeJulia <92155225+PenelopeJulia@users.noreply.github.com>
2022-01-25 00:34:57 +0100

    Karten abbilden

--- script/script.ts
+++ script/script.ts
@@ -2,71 +2,67 @@ namespace endabagabe {
     
 // Interface für die Karten erstellt
 interface cards {
-    // Wert der Karten ald Rick & Morty-Charaktere (Ass, König, Dame, Bube, 10, 9, 8, 7); Werte sind in der README-Datei erklärt
-    cardValue: number;
+    // Wert der Karten (Ass, König, Dame, Bube, 10, 9, 8, 7)
+    numberOfCards: number;
     // Farbe der Karten (blau, rot, gelb, grün)
-    cardColor: string;
+    valueOfCards: string;
 }
 
 // Leere Arrays für die unterschiedlichen Kartenstapel, so dass bei neu Start beim leeren der Arrays das Spiel ohne Probleme von Scratch neu angefangen werden kann
-let drawCardStack: cards[] = []; // Array für den Ziehstapel
-let playCardStack: cards[] = []; // Array für den Ablagestapel
+let drawStack: cards[] = []; // Array für den Ziehstapel
+let playStack: cards[] = []; // Array für den Ablagestapel
 let cardsHuman: cards[] = []; // Karten des Spielers
-let cardsComputer: cards[]; // Karten des Computers
+let cardsComputer: cards[] = []; // Karten des Computers
 
-let initialCards: number = 3;
-
-let value: string[] = ["A", "K", "D", "B", "10", "9", "8", "7"];
-let color: string[] = ["blue", "green", "red", "yellow"];
+// Karten bekommen einen Nummern-Wert zugeschrieben in einem Array
+let number: string[] = ["7", "8", "9", "10", "Bube", "Dame", "König", "Ass"];
+// Array, das alle Karten beinhaltet 
 let allCards = createCards();
-
-// Variablen deklariert, so dass später mit ihnen gearbeitet werden kann
 
 let cardFitsCriteria: boolean = false; // Üperfrüfen, ob Karte, die gespielt wird mit der obersten Karte auf dem Ablagestapel übereinstimmt
 let topPlayCardStack: boolean = false; // Oberste Karte vom Ablagestapel
-let cardComputer: cards; // Karte, die der Computer legt, falls Bedingung erfüllt(Karte muss den gleichen Wert oder Farbe der obersten KArte im Ablagestapel besitzen) 
+//let cardComputer: cards; // Karte, die der Computer legt, falls Bedingung erfüllt(Karte muss den gleichen Wert oder Farbe der obersten KArte im Ablagestapel besitzen) 
 
 // Bei Klick auf Start wird der Eventlistener ausgelöst und die Funktion startGame aufgerufen
 let start: HTMLElement = document.querySelector("#startGame");
 start.addEventListener("click", startGame);
 
+//Funktion, die andere Funktionen wie das Shufflen der Karten und austeilen der Kartena aufruft
 function startGame(): void {
+//clearHTML();
 shuffleCards(allCards);
+dealCards();
 console.log(allCards);
+
+
 }
 
 // In Gruppenarbeit mit Celine entstanden
 
 // Den Karten wird der zugehörige Wert (Rick & Morty Charaktere) und die Farben (blau, grün, rot, gelb) zugeordnet
+// Klick auf Start löst startGame-Funktion aus und diese triggert wiederum die createCars-Funktion
 function createCards() {
 
+// Farbe und Wert der Karten werden deklariert und typisiert
     let cardValue: string;
-
     let allCards = [];
 
-    for (let v = 0; v < value.length; v++) {
-        if (v == 0) {
-            cardValue = "ace";
+    for (let v = 0; v < 4; v++) {
+        if (v == 0) { 
+            cardValue = "blueCard"; // Wenn cardValue = 0 ist dann hat die Karte den Wert "blau"
         } else if (v == 1) {
-            cardValue = "king";
+            cardValue = "greenCard";
         } else if (v == 2) {
-            cardValue = "queen";
+            cardValue = "redCard";
         } else if (v == 3) {
-            cardValue = "jack";
-        } else if (v == 4) {
-            cardValue = "10";
-        } else if (v == 5) {
-            cardValue = "9";
-        } else if (v == 6) {
-            cardValue = "8";
-        } else if (v == 7) {
-            cardValue = "7";
+            cardValue = "yellowCard";
         }
-
-        for (let c = 0; c < color.length; c++) {
-           let valueCards: string = value[v];
-           let colorCards: string = color[c];
-           allCards.push({valueCards, colorCards});
+//
+        for (let c = 0; c < 9; c++) {
+           let valueOfCards: string = cardValue; 
+           let numberOfCards: string = number[c]; // Wenn color = 0, dann ist die Karte Blau, da im "color"- array Blau in der Position 0 ist
+           
+           allCards.push({valueOfCards, numberOfCards});
        }
         console.log("value");
     }
@@ -83,12 +79,68 @@ function shuffleCards(allCards): void {
 
         allCards[location1] = allCards[location2];
         allCards[location2] = tmp;
-
     }
+    // Deal Cards
+    createDrawStack();
+    createPlayCardStack();
+    generatePlayerCards();
+    generateComputerCards();
+}
+
+function dealCards(): void {
+
+    // Hier wird auf das Div mit der ID = Computer zugegriffen
+    let computerCards = document.getElementById("computer");
+    // Hier wird auf das Div mit dem ID = Human zugegriffen
+    let humanCards = document.getElementById("human");
+
+    let drawCardStack = document.getElementById("drawCardStack");
+
+    let playCardStack = document.getElementById("playCardStack");
+
+
+    // For-Schleife, die die Computer-Karten mit der jeweiligen Kartenfarbe an das neu erstellte Div hängt
+    for (let i: number = 0; i < 3; i++) {
+        // Neues DIV-Element erstellen
+        let computerDiv: HTMLElement = document.createElement("div"); 
+        computerDiv.className = cardsComputer[i].valueOfCards; // NewDiv wird eine Klasse zugeschrieben und die Karten-Eigenschaften übergegeben
+        console.log(cardsComputer[0]); //Console.log, um zu überprüfen, ob die Karten im HTML erscheinen
+        computerCards.appendChild(computerDiv); // Die drei Karten, die der Computer-Hand geshuffelt in der Funktion generateComputerCards zugeteilt werden, werden nun an das neue newDiv-Element, dass sich im computer-div befindet, übergegeben und erscheinen auf der Seite
+        
+        let humanDiv: HTMLElement = document.createElement("div");
+        humanDiv.className = cardsHuman[i].valueOfCards;
+        humanCards.appendChild(humanDiv);
+    }
+
+    for(let i: number = 0; i < 1; i++){
+        let discardCardStack: HTMLElement = document.createElement("div");
+        discardCardStack.className = 
+    }
+   
+}
+
+function createDrawStack(): void {
+    
+console.log(allCards[0].valueOfCards);
+}
+
+function createPlayCardStack(): void {
+
+}
+
+function generatePlayerCards(): void {
+    cardsHuman.push(allCards[3], allCards[4], allCards[5] );
+    console.log(cardsHuman[0]);
+}
+
+function generateComputerCards(): void {
+    cardsComputer.push(allCards[0], allCards[1], allCards[2] );
 }
 
 
-//function discard;
+function cleatHTML(): void {
 
+
+}
 
 }
